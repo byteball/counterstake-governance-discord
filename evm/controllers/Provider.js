2cab14a9313f92d83256042ff732c6218038fd38
Evgenii Stulnikov <xxjenekxx@gmail.com>
2026-05-31 21:35:20 +0300

    feat: implement heartbeat mechanism for WebSocket connection management and empty data checker

--- evm/controllers/Provider.js
+++ evm/controllers/Provider.js
@@ -4,10 +4,16 @@ const EventEmitter = require('node:events');
 
 const sleep = require('../../utils/sleep');
 
+const HEARTBEAT_INTERVAL = 30 * 1000;
+const PONG_TIMEOUT = 10 * 1000;
+const WS_OPEN = 1;
+
 class Provider {
 	#network;
 	#url;
 	#connectCB;
+	#heartbeatInterval = null;
+	#pongTimeout = null;
 	
 	_provider = null;
 	events = new EventEmitter();
@@ -42,6 +48,7 @@ class Provider {
 	
 	close() {
 		if (!this._provider || this._provider.destroyed) return;
+		this.#stopHeartbeat();
 		this._provider.websocket.removeAllListeners();
 		this._provider.destroy();
 	}
@@ -59,22 +66,63 @@ class Provider {
 		this._provider.websocket.on('error', (error) => {
 			this.#onError(error);
 		});
+		this._provider.websocket.on('pong', () => {
+			this.#onPong();
+		});
 	}
 
 	#onOpen() {
+		this.#startHeartbeat();
 		this.#connectCB();
 	}
 
 	#onError(error) {
 		console.error(`[Provider[${this.#network}].ws_error]:`, error);
+		this.#stopHeartbeat();
 		this.close();
 	}
 
 	async #onClose(code) {
+		this.#stopHeartbeat();
 		console.error(`[Provider[${this.#network}].ws_close]:`, code);
 		this.events.emit('close');
 		await sleep(2);
 		this.connect();
+	}
+
+	#startHeartbeat() {
+		this.#stopHeartbeat();
+		const websocket = this._provider.websocket;
+		if (typeof websocket.ping !== 'function') return;
+
+		this.#heartbeatInterval = setInterval(() => this.#sendPing(), HEARTBEAT_INTERVAL);
+		this.#heartbeatInterval.unref?.();
+		this.#sendPing();
+	}
+
+	#stopHeartbeat() {
+		clearInterval(this.#heartbeatInterval);
+		clearTimeout(this.#pongTimeout);
+		this.#heartbeatInterval = null;
+		this.#pongTimeout = null;
+	}
+
+	#sendPing() {
+		const websocket = this._provider.websocket;
+		if (websocket.readyState !== WS_OPEN) return;
+
+		clearTimeout(this.#pongTimeout);
+		this.#pongTimeout = setTimeout(() => {
+			console.error(`[Provider[${this.#network}].ws_pong_timeout]`);
+			(websocket.terminate || websocket.close).call(websocket);
+		}, PONG_TIMEOUT);
+		this.#pongTimeout.unref?.();
+		websocket.ping();
+	}
+
+	#onPong() {
+		clearTimeout(this.#pongTimeout);
+		this.#pongTimeout = null;
 	}
 }
 
